e45919026f88dcbe95fb741a5d00f19bdac45c51
Arthur <yuanfeng0905@gmail.com>
2019-09-09 17:23:24 +0800

    fix ver

--- tool/kratos/template.go
+++ tool/kratos/template.go
@@ -546,7 +546,7 @@ type Kratos struct {
 go 1.12
 
 require (
-	github.com/yuanfeng0905/oasis-kratos v0.2.2
+	github.com/yuanfeng0905/oasis-kratos v1.0.0
 	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.3.2
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
